f978085da3bdc17b87792d65ba763d2b3d4b0cc9
jonathan-jota <jonathan.jota.santos@gmail.com>
2022-04-12 23:33:04 -0300

    Adicionando o Spring

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -2,8 +2,8 @@ package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +15,22 @@ import java.util.List;
 @RestController
 @RequestMapping("anime")
 @Slf4j
+@RequiredArgsConstructor//gera um constructor com todos os atributos do tipo final
 public class AnimeController {
 
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
-    //@Autowired
-    public AnimeController(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
-    };
+//    @Autowired
+//    public AnimeController(DateUtil dateUtil) {
+//        this.dateUtil = dateUtil;
+//    };
 
     @GetMapping(path="/list")
     public List<Anime> listAll() {
         log.info("Date Formatted {}", dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return Arrays.asList(new Anime ("DBZ"), new Anime("Berserk"));
+        return Arrays.asList(
+                new Anime ("Naruto"),
+                new Anime("DBZ"),
+                new Anime("CDZ"));
     }
 }
--- src/main/java/academy/devdojo/springboot2/util/DateUtil.java
+++ src/main/java/academy/devdojo/springboot2/util/DateUtil.java
@@ -1,8 +1,11 @@
 package academy.devdojo.springboot2.util;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class DateUtil {
 
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
